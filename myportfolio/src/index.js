3662011db251616d5d5c5a25ab02f8bbf56dcf18
PankajLamgria <pankajlamgria@gmail.com>
2024-08-31 09:51:15 +0530

    added document title

--- myportfolio/src/index.js
+++ myportfolio/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+document.title="Pankaj Lamgria"
 root.render(
   <React.StrictMode>
     <App />
